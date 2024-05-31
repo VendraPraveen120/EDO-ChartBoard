89418908bad117ffa4a176888c766174c859f6b3
VendraPraveen120 <Praveen.vendra.144@gmail.com>
2024-05-31 12:33:01 +0530

    add some code

--- sql_apis/index.js
+++ sql_apis/index.js
@@ -21,6 +21,10 @@ const connection = mysql.createPool({
 // });
 
 
+sqlRoute.get("/",(req,res)=>{
+  res.send("helloworld")
+})
+
 sqlRoute.post('/query', (req, res) => {
     const query = req.body.query;
   
